1ae6db5b6a88540a5a4e95d07a64d5358afe6866
mostnour <mostnour@yahoo.com>
2024-10-13 14:56:00 +0300

    Add Business Unit to Login screen

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -19,6 +19,8 @@ class _LoginState extends State<Login> {
     super.initState();
   }
 
+  String? selectedCountry; // = "USA";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,6 +59,105 @@ class _LoginState extends State<Login> {
                         borderSide: BorderSide(width: 1),
                       )),
                 ),
+                // Bussines Unit
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                    child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 40),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          border: Border.all(
+                              color: Colors.black26,
+                              style: BorderStyle.solid,
+                              width: 1 /*0.80*/),
+                        ),
+                        /*color:
+                      const Color.fromARGB(255, 149, 193, 197),*/ //Colors.blue,
+                        child: /*DropdownButtonHideUnderline(
+                    child:*/
+                            Column(
+                          children: [
+                            Text(
+                              "وحدة العمل",
+                              textDirection: TextDirection.rtl,
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                  color: Color.fromARGB(160, 0, 0, 0),
+                                  fontSize: 20),
+                            ),
+                            DropdownButton(
+                              onTap: () {
+                                print("You tapped the menu :)");
+                              },
+                              itemHeight: 50,
+                              /*style: TextStyle(
+                      //color: Colors.black,
+                      backgroundColor: const Color.fromARGB(
+                          255, 149, 193, 197), //Colors.white,
+                    ),*/
+                              icon: Icon(Icons
+                                  .arrow_drop_down_circle /*, color: Colors.red,*/),
+                              iconEnabledColor: const Color.fromARGB(
+                                  255, 33, 150, 243), //Colors.yellow,
+                              iconDisabledColor: Colors.red,
+                              iconSize: 40,
+                              dropdownColor: Colors.pink[50], //Colors.grey,
+                              underline: Divider(
+                                thickness:
+                                    0, /*color: Colors.red, thickness: 10,*/
+                              ), // or use DropdownButtonHideUnderline
+                              isExpanded: true,
+                              hint: Container(
+                                padding: EdgeInsets.only(right: 10),
+                                width: double.infinity,
+                                child: const Text(
+                                  "اختر وحدة العمل من هنا",
+                                  textDirection: TextDirection.rtl,
+                                  textAlign: TextAlign.right,
+                                  style: TextStyle(
+                                      color: Colors.pink, fontSize: 20),
+                                ),
+                              ),
+                              items: [
+                                "المركز الرئيسي",
+                                "رصيف التحميل",
+                                "الفرع",
+                                "الصيانة",
+                                "محطة التمويل"
+                              ]
+                                  .map((e) => DropdownMenuItem(
+                                        child: Container(
+                                            width: double.infinity,
+                                            color: Colors.pink[50],
+                                            child: Text(
+                                              "$e",
+                                              style: TextStyle(
+                                                  color: Colors.brown,
+                                                  fontSize: 20),
+                                            )),
+                                        value: e,
+                                      ))
+                                  .toList(),
+                              onChanged: (String? val) {
+                                setState(() {
+                                  selectedCountry = val;
+                                });
+                                print("Drop down menu changed to $val");
+                              },
+                              value: selectedCountry,
+                            ),
+                          ],
+                        )
+
+                        // ), DropdownButtonHideUnderline
+                        )),
+                const SizedBox(
+                  height: 20,
+                ),
+                // Bussines Unit
                 Container(
                   margin: const EdgeInsets.all(10),
                   child: Row(
